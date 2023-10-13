f3d3c27090b1c107f6a8df1da98ab670dd3d7be4
lvalidal <tu6jp2w4h@code.edu.az>
2023-10-13 11:51:08 +0400

    ok

--- main.js
+++ main.js
@@ -98,24 +98,27 @@
 
 
 
-let arr = [3, 8, 2, 5 , 4 , 99 , 10, 7 , 6 ] ;
+// let arr = [3, 8, 2, 5 , 4 , 99 , 10, 7 , 6 ] ;
 
-let max = arr[0] ;
+// let max = arr[0] ;
 
-console.log(arr);
+// console.log(arr);
 
-for (let i= 0; index < arr.length; i++) {
+// for (let i= 0; index < arr.length; i++) {
     
 
-    if (arr[i] > max && arr %2 === 0) {
+//     if (arr[i] > max && arr %2 === 0) {
 
-                max = arr[i] ;
+//                 max = arr[i] ;
                 
-            }
+//             }
     
-}
+// }
 
-console.log("max=" , max)
+// console.log("max=" , max)
+
+
+
 
 
 
