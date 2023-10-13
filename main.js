19867cdb876f7838bc44967f56977584f70a2069
lvalidal <tu6jp2w4h@code.edu.az>
2023-10-13 11:07:47 +0400

    tasks done at uni

---
+++ main.js
@@ -0,0 +1,121 @@
+// let index = (Number(prompt("enter your number!"))) ;
+
+
+
+// for (let index = 10; index < 100; index++) {
+    
+
+//     if ( index % 10 === 7 ) {
+
+
+
+//         alert( index ) ;
+       
+        
+//     }
+// }
+
+
+
+// let index = (Number(prompt("enter your number!"))) ;
+
+
+
+// for (let index = 10; index < 100; index++) {
+    
+
+//     if ( index % 11 === 0 ) {
+
+
+
+//         alert( index ) ;
+       
+        
+//     }
+// }
+
+
+
+// let a = prompt("enter your number!")
+
+
+// for (let i = 0; i < a.length; i++) {
+   
+    
+// console.log(a[i]);
+// };
+
+
+
+// let a = (prompt("enter your number!") )  ;
+
+
+// let count = 0 ;
+
+// for (let i = 0; i < a.length; i++) {
+   
+//     count = a[i] ;
+
+//     if (a[i] > count) {
+
+//         count = a[i] ;
+        
+//     }
+
+// };
+
+
+// console.log(count)
+
+
+
+// let numbers = (prompt("enter your number!"));
+
+
+
+// let sum = 0;
+
+// let hs = 1 ;
+
+
+// for (let index = 0; index < numbers.length; index++) {
+    
+
+    
+//     sum += Number(numbers[index]) ;
+
+//     hs *= Number(numbers[index]) ;
+    
+// }
+
+// alert( ` sum is ${sum} `) ;
+
+// alert(`hasil = ${hs}`) ;
+
+// let result = sum / (numbers.length) ;
+
+// alert(`ededi orta = ${result}`)
+
+
+
+let arr = [3, 8, 2, 5 , 4 , 99 , 10, 7 , 6 ] ;
+
+let max = arr[0] ;
+
+console.log(arr);
+
+for (let i= 0; index < arr.length; i++) {
+    
+
+    if (arr[i] > max && arr %2 === 0) {
+
+                max = arr[i] ;
+                
+            }
+    
+}
+
+console.log("max=" , max)
+
+
+
